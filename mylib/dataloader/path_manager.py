33a1ed71df431298e7c9c7dab492fd77d1cc7f63
FakeHermes <42340043+FakeHermes@users.noreply.github.com>
2020-01-03 22:18:48 +0800

    Update path_manager.py

--- mylib/dataloader/path_manager.py
+++ mylib/dataloader/path_manager.py
@@ -17,8 +17,18 @@ class PathManager:
         return df
 
     @property
+    def test(self):
+        import pandas as pd
+        df1 = pd.read_csv(os.path.join('D:/ProgramData/github/ML_hw/3D-medical-image-classification/mylib/dataloader/test.csv'))
+        return df1
+
+    @property
     def nodule_path(self):
-        return os.path.join('D:/ProgramData/github/ML_hw/3D-medical-image-classification/train_val')
+        return os.path.join('D:/ProgramDataKaglle/new-data/train_val')
+    
+    @property
+    def test_nodule_path(self):
+        return os.path.join('D:/ProgramData/Kaglle/new-data/test')
 
 
 def parse_environ(cfg_path=None):
